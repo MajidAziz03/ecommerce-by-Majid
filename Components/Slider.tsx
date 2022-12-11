ae006590160f87210aff59d883e6f56ab746d81f
Majid Aziz <azizmajid34@gmail.com>
2022-12-11 12:33:32 +0500

    created slider without functionalities

--- Components/Slider.tsx
+++ Components/Slider.tsx
@@ -1,45 +1,104 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@mui/icons-material'
+import { Button } from '@mui/material'
 import React from 'react'
 import styled from 'styled-components'
 
-
-
 const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    background-color: coral;
-    position: relative;
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+`;
 
 const Arrow = styled.div`
-    width : 50px;
-    height: 50px;
-    background-color: #fff7f7;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    cursor: pointer;
-    margin: auto;
-    left : ${props => props.direction === "left" && "10px"};
-    right: ${props => props.direction === "right" && "10px"};
-`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+// const Button = styled.button`
+//   padding: 10px;
+//   font-size: 20px;
+//   background-color: transparent;
+//   cursor: pointer;
+// `;
+
+
 
 const Slider = () => {
-  return (
-      <Container>
-          <Arrow direction = "left">
-              <ArrowLeftOutlined />
-          </Arrow>
-          <Arrow direction = "right">
-              <ArrowRightOutlined />
-          </Arrow>
-    </Container>
-  )
+    return (
+        <Container>
+            <Arrow direction="left">
+                <ArrowLeftOutlined />
+            </Arrow>
+            <Wrapper>
+                <Slide>
+                    <ImgContainer>
+                        <Image src='/img/pizza.png' />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title> Summer Sale </Title>
+                        <Desc> DON'T COMPROMISE ON QUALITY FLAT 30% OFF ON ALL PRODUCTS </Desc>
+                        <Button variant='contained' color='primary' size='sm' > Shop Now!</Button>
+                    </InfoContainer>
+                </Slide>
+            </Wrapper>
+            <Arrow direction="right">
+                <ArrowRightOutlined />
+            </Arrow>
+        </Container>
+    )
 }
 
 export default Slider
